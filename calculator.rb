13ad4156e4a4a8e5d18bf67d07bdc98b254c265c
Tom Lamers <skilletfan59@yahoo.com>
2016-06-02 16:13:15 -0500

    Hopefully final version - added shortcuts and fixed astetics

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+class Functions	
 	def add (x,y)
 		x + y
 	end
@@ -23,7 +24,17 @@
 		end
 	end
 	def divide (x,y)
-		x.to_f / y.to_f
+		z = x.to_f / y.to_f
+		if z.to_s == "Infinity"
+			puts "You cannot divide numbers by 0!"
+		else
+			z
+		end
+	end
+	def remainder (x,y)
+		$d = x.to_i / y.to_i
+		$r = x.to_i % y.to_i
+	rescue ZeroDivisionError
 	end
 	def power (x,y)
 		x ** y
@@ -41,128 +52,168 @@
 		end
 		i
 	end
-print %x{clear}
-puts "This program is a calculator emulator. Your options for
+end
+class Calculator
+	def initialize
+		print %x{clear}
+		puts "This program is a calculator emulator. Your options for
 functions are: ADD - to add two numbers, SUBTRACT - to subtract
 the first number from the second, SUM - to add multiple numbers,
 MULTIPLY - to multiply any amount of numbers together, DIVIDE - 
 to divide the first number by the second number, POWER - to 
 multiply the first number by the power of the second number,
 FACTORIAL - all the numbers between that one and 1 multiplied"
-inputs = ["add", "subtract", "sum", "multiply", "divide", "power", "factorial", "exit"]
-input = nil
-while input != "exit"
-input = "nothing"
-until inputs.include?(input) == true do
-	puts
-	puts "Options are: add, subtract, sum, multiply, divide, power, factorial, exit"
-	print "What function would you like to do? "
-	input = gets.chomp.downcase
-end
-numbers = []
-x = nil
-y = nil
-n = nil
-b = "nothing"
-result = "The answer is: "
-clear = %x{clear}
-if input == "exit"
-	print clear
-	exit
-elsif input == "add" || input == "subtract" || input == "power" || input == "divide"
-	until x.is_a?(Fixnum)
-		puts
-		print "Please enter the first number: "
-		x = gets.chomp
-		if
-			begin
-				Float(x)
-				x = x.to_f
-				break
-			rescue ArgumentError
+		@c = Functions.new
+	end
+	def input
+		inputs = ["add", "subtract", "sum", "multiply", "divide", "power", "factorial", "exit", "a", "s", "sub", "m", "d", "p", "f", "+", "++", "-", "*", "^", "/", " "]
+		input = nil
+		while input != " "
+			input = "nothing"
+			until inputs.include?(input) == true do
+				puts
+				puts "Options are: add, subtract, sum, multiply, divide, power, factorial, exit"
+				print "What function would you like to do? "
+				input = gets.chomp.downcase
 			end
-		else
-			puts "That's not a number!"
-		end
-	end
-	#until x.is_a?(Fixnum) do
-		#puts
-		#print "Please enter the first number: "
-		#x = Integer(gets) rescue nil
-	#end
-	until y.is_a?(Fixnum)
-		puts
-		print "Please enter the second number: "
-		y = gets.chomp
-		if
-			begin
-				Float(y)
-				y = y.to_f
-				break
-			rescue ArgumentError
+			numbers = []
+			x = nil
+			y = nil
+			n = nil
+			b = "nothing"
+			result = "The answer is: "
+			clear = %x{clear}
+			if input == "exit" || input == " "
+				print clear
+			elsif input == "add" || input == "subtract" || input == "power" || input == "divide" || input == "a" || input == "sub" || input == "p" || input == "d" || input == "+" || input == "-" || input == "/" || input == "^"
+				until x.is_a?(Fixnum)
+					puts
+					print "Please enter the first number: "
+					x = gets.chomp
+					if
+						begin
+							Float(x)
+							x = x.to_f
+							break
+						rescue ArgumentError
+						end
+					else
+						puts "That's not a number!"
+					end
+				end
+				#until x.is_a?(Fixnum) do       #only allows for integers
+					#puts
+					#print "Please enter the first number: "
+					#x = Integer(gets) rescue nil
+				#end
+				until y.is_a?(Fixnum)
+					puts
+					print "Please enter the second number: "
+					y = gets.chomp
+					if
+						begin
+							Float(y)
+							y = y.to_f
+							break
+						rescue ArgumentError
+						end
+					else
+						puts "That's not a number!"
+					end
+				end
+				#until y.is_a?(Fixnum) do    #only allows for integers
+					#puts
+					#print "Please enter the second number: "
+					#y = Integer(gets) rescue nil
+				#end
+				if input == "add" || input == "a" || input == "+"
+					puts clear
+					print result
+					z = @c.add(x,y)
+					if z == Integer(z)
+						z = z.to_i
+					end
+					puts z
+				elsif input == "subtract" || input == "sub" || input == "-"
+					puts clear
+					print result
+					z = @c.subtract(x,y)
+					if z == Integer(z)
+						z = z.to_i
+					end
+					puts z
+				elsif input == "power" || input == "p" || input == "^"
+					puts clear
+					print result
+					z = @c.power(x,y)
+					if z == Integer(z)
+						z = z.to_i
+					end
+					puts z
+				elsif input == "divide" || input == "d" || input == "/"
+					puts clear
+					print result
+					z = @c.divide(x,y)
+					if z != z.to_i
+						@c.remainder(x,y)
+						if $r != nil
+							puts z.to_s + " or #{$d} with a remainder of #{$r}"
+						end
+					else
+						if z == z.to_i
+							z = z.to_i
+						end
+						puts z
+					end
+				end
+			elsif input == "multiply" || input == "sum" || input == "m" || input == "s" || input == "*" || input == "++"
+				puts
+				puts "Enter as many numbers as you want to separately
+then enter 'done' when you are finished."
+				while b != " "
+					puts
+					print "Please enter a number: "
+					b = gets.chomp
+					if b == " " || b == "done"
+						b = " "
+					elsif
+						begin 
+							Float(b)
+						numbers.push Float(b)
+						rescue ArgumentError
+						end	
+					else
+						puts "That's not a number!"
+					end
+				end
+				if input == "multiply" || input == "m" || input == "*"
+					puts clear
+					print result
+					z = @c.multiply(numbers)
+					if z == Integer(z)
+						z = z.to_i
+					end
+					puts z
+				else input == "sum" || input == "s" || input == "++"
+					puts clear
+					print result
+					z = @c.sum(numbers)
+					if z == Integer(z)
+						z = z.to_i
+					end
+					puts z
+				end
+			elsif input == "factorial" || input == "f"
+				until n.is_a?(Fixnum) do
+					puts
+					print "Please enter a number: "
+					n = Integer(gets) rescue nil
+				end
+				puts clear
+				print result
+				puts @c.factorial(n)
 			end
-		else
-			puts "That's not a number!"
 		end
 	end
-	#until y.is_a?(Fixnum) do
-		#puts
-		#print "Please enter the second number: "
-		#y = Integer(gets) rescue nil
-	#end
-	if input == "add"
-		puts clear
-		print result
-		puts add(x,y)
-	elsif input == "subtract"
-		puts clear
-		print result
-		puts subtract(x,y)
-	elsif input == "power"
-		puts clear
-		print result
-		puts power(x,y)
-	elsif input == "divide"
-		puts clear
-		print result
-		puts divide(x,y)
-	end
-elsif input == "multiply" || input == "sum"
-	puts
-	puts "Enter as many numbers as you want to separately
-	then enter 'done' when you are finished."
-	while b != "done"
-		puts
-		print "Please enter a number: "
-		b = gets.chomp
-		if b == "done"
-		elsif
-			begin 
-				Float(b)
-			numbers.push Float(b)
-			rescue ArgumentError
-			end	
-		else
-			puts "That's not a number!"
-		end
-	end
-	if input == "multiply"
-		puts clear
-		print result
-		puts multiply(numbers)
-	else input == "sum"
-		puts clear
-		print result
-		puts sum(numbers)
-	end
-else input == "factorial"
-	until n.is_a?(Fixnum) do
-		puts
-		print "Please enter a number: "
-		n = Integer(gets) rescue nil
-	end
-	puts clear
-	print result
-	puts factorial(n)
 end
-end
+Calculator.new.input
