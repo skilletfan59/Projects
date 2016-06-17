8c32ceac44d1f716779b899de1f2b2ae78a24b88
Tom Lamers <skilletfan59@yahoo.com>
2016-06-16 19:15:53 -0500

    fixed global varialbes

--- Calculator Programs/objectcalc.rb
+++ Calculator Programs/objectcalc.rb
@@ -1,44 +1,46 @@
+CLEAR = %x{clear}
 class Calculator
-	def initialize
-		$clear = %x{clear}
-		$result = "The answer is: "
-		$input = nil
-		$g = Input.new
-		print $clear
-		puts "This program is a calculator emulator. Your options for
-functions are: ADD - to add two numbers, SUBTRACT - to subtract
-the first number from the second, SUM - to add multiple numbers,
-MULTIPLY - to multiply any amount of numbers together, DIVIDE - 
-to divide the first number by the second number, POWER - to 
-multiply the first number by the power of the second number,
-FACTORIAL - all the numbers between that one and 1 multiplied"
-		while $input != " "
-			$g.main
-			Operations.new
-		end
+	def explain
+		print CLEAR
+		puts " This program is a calculator emulator. Your options for
+ functions are: ADD - to add two numbers, SUBTRACT - to subtract
+ the first number from the second, SUM - to add multiple numbers,
+ MULTIPLY - to multiply any amount of numbers together, DIVIDE - 
+ to divide the first number by the second number, POWER - to 
+ multiply the first number by the power of the second number,
+ FACTORIAL - all the numbers between that one and 1 multiplied"
+	end
+	def run
+		input = Input.new.reset
+		input = Input.new.main
+		Operations.new.do(input)
 	end
 end
 class Input
 	def main
+		input = "nothing"
 		inputs = ["add", "subtract", "sum", "multiply", "divide", "power", "factorial", "exit", "a", "s", "sub", "m", "d", "p", "f", "+", "++", "-", "*", "^", "/", " ", "!"]
-		until inputs.include?($input) == true do
+		until inputs.include?(input)
 			puts
 			puts "Options are: add, subtract, sum, multiply, divide, power, factorial, exit"
 			print "What function would you like to do? "
-			$input = gets.chomp.downcase
+			input = gets.chomp.downcase
+			if inputs.include?(input) == false
+				puts "That is not a valid option! Please try again."
+			end
 		end
+		input
 	end
-	def duoin
-		$x = nil
-		$y = nil
-		until $x.is_a?(Fixnum)
+	def getx
+		x = nil
+		until x.is_a?(Fixnum)
 			puts
 			print "Please enter the first number: "
-			$x = gets.chomp
+			x = gets.chomp
 			if
 				begin
-					Float($x)
-					$x = $x.to_f
+					Float(x)
+					x = x.to_f
 					break
 				rescue ArgumentError
 				end
@@ -46,14 +48,18 @@ class Input
 				puts "That's not a number!"
 			end
 		end
-		until $y.is_a?(Fixnum)
+		x
+	end
+	def gety
+		y = nil
+		until y.is_a?(Fixnum)
 			puts
 			print "Please enter the second number: "
-			$y = gets.chomp
+			y = gets.chomp
 			if
 				begin
-					Float($y)
-					$y = $y.to_f
+					Float(y)
+					y = y.to_f
 					break
 				rescue ArgumentError
 				end
@@ -61,110 +67,150 @@ class Input
 				puts "That's not a number!"
 			end
 		end
+		y
 	end
 	def multin
 		b = "nothing"
-		$numbers = []
+		numbers = []
 		puts
 		puts "Enter as many numbers as you want to separately
-then enter 'done' when you are finished."
+ then enter 'done' when you are finished."
 		while b != " "
 			puts
 			print "Please enter a number: "
 			b = gets.chomp
 			if b == " " || b == "done"
 				b = " "
+				break
 			elsif
 				begin 
 					Float(b)
-				$numbers.push Float(b)
+				numbers.push Float(b)
 				rescue ArgumentError
 				end	
 			else
 				puts "That's not a number!"
 			end
 		end
+		numbers
 	end
 	def singin
-		$n = nil
-		until $n.is_a?(Fixnum) do
+		n = nil
+		until n.is_a?(Fixnum) do
 			puts
 			print "Please enter a number: "
-			$n = Integer(gets) rescue nil
+			n = Integer(gets) rescue nil
 		end
+		n
 	end
 	def reset
-		$input = nil
+		nil
 	end
 end
-class Result
-	def initialize(z)
-		print $clear
-		print $result
+class Operations
+	def do(input)
+		case input
+		when "add", "a", "+"
+			x = Input.new.getx
+			y = Input.new.gety
+			z = Add.new.add(x,y)
+			result(z)
+			Calculator.new.run
+		when "subtract", "sub", "-"
+			x = Input.new.getx
+			y = Input.new.gety
+			z = Subtract.new.minus(x,y)
+			result(z)
+			Calculator.new.run
+		when "power", "p", "^"
+			x = Input.new.getx
+			y = Input.new.gety
+			z = Power.new.power(x,y)
+			result(z)
+			Calculator.new.run
+		when "divide", "d", "/"
+			x = Input.new.getx
+			y = Input.new.gety
+			g = Divide.new
+			g.divide(x,y)
+			g.result(x,y)
+			Calculator.new.run
+		when "multiply", "m", "*"
+			numbers = Input.new.multin
+			z = Multiply.new.times(numbers)
+			result(z)
+			Calculator.new.run
+		when "sum", "s", "++"
+			numbers = Input.new.multin
+			z = Sum.new.sum(numbers)
+			result(z)
+			Calculator.new.run
+		when "factorial", "f", "!"
+			n = Input.new.singin
+			z = Factorial.new.fact(n)
+			result(z)
+			Calculator.new.run
+		when " ", "exit"
+			print CLEAR
+		end
+	end
+	def result(z)
+		print CLEAR
+		print "The answer is: "
 		if z == Integer(z)
 			z = z.to_i
 		end
 		puts z
 	end
 end
-class Add
-	def initialize
-		@z = $x + $y
-	end
-	def result
-		Result.new(@z)
+class Add < Operations
+	def add(x,y)
+		x + y
 	end
 end
-class Subtract
-	def initialize
-		@z = $x - $y
-	end
-	def result
-		Result.new(@z)
+class Subtract < Operations
+	def minus(x,y)
+		x - y
 	end
 end
-class Sum
-	def initialize
-		@n = 0
-		if $numbers == []
+class Sum < Operations
+	def sum(numbers)
+		n = 0
+		if numbers == []
 			0
 		else
-		$numbers.each {|a| @n += a}
+		numbers.each {|a| n += a}
 		end
-	end
-	def result
-		Result.new(@n)
+		n
 	end
 end
-class Multiply
-	def initialize
-		@n = 1
-		if $numbers == []
+class Multiply < Operations
+	def times(numbers)
+		n = 1
+		if numbers == []
 			0
 		else
-		$numbers.each {|a| @n *= a}
+		numbers.each {|a| n *= a}
 		end
-	end
-	def result
-		Result.new(@n)
+		n
 	end
 end
-class Divide
-	def initialize
-		@z = $x.to_f / $y.to_f
+class Divide < Operations
+	def divide(x,y)
+		@z = x.to_f / y.to_f
 		if @z.to_s == "Infinity"
 			@z = "Undefined
-You cannot divide numbers by 0!"
+ You cannot divide numbers by 0!"
 		end
-		@d = $x.to_i / $y.to_i
-		@r = $x.to_i % $y.to_i
+		@d = x.to_i / y.to_i
+		@r = x.to_i % y.to_i
 	rescue ZeroDivisionError
 	end
-	def result
-		print $clear
-		print $result
+	def result(x,y)
+		print CLEAR	
+		print "The answer is: "
 		if @z != @z.to_i
-			if $x == $x.to_i && $y == $y.to_i
+			if x == x.to_i && y == y.to_i
 				if @r != nil
 					puts @z.to_s + "  -or-  #{@d} with a remainder of #{@r}"
 				else
@@ -181,65 +227,25 @@ You cannot divide numbers by 0!"
 		end
 	end
 end
-class Power
-	def initialize
-		@z = $x ** $y
-	end
-	def result
-		Result.new(@z)
+class Power < Operations
+	def power(x,y)
+		x ** y
 	end
 end
-class Factorial
-	def initialize
-		@i = 1
-		@i *= $n
-		while $n > 1
-			$n -= 1
-			@i *= $n
+class Factorial < Operations
+	def fact(n)
+		i = 1
+		i *= n
+		while n > 1
+			n -= 1
+			i *= n
 		end
-		if @i == 0
-			@i = 1
+		if i == 0
+			i = 1
 		end
-	end
-	def result
-		Result.new(@i)
+		i
 	end
 end
-class Operations
-	def initialize
-		case $input
-		when "add", "a", "+"
-			Input.new.duoin
-			Add.new.result
-			$g.reset
-		when "subtract", "sub", "-"
-			Input.new.duoin
-			Subtract.new.result
-			$g.reset
-		when "power", "p", "^"
-			Input.new.duoin
-			Power.new.result
-			$g.reset
-		when "divide", "d", "/"
-			Input.new.duoin
-			Divide.new.result
-			$g.reset
-		when "multiply", "m", "*"
-			Input.new.multin
-			Multiply.new.result
-			$g.reset
-		when "sum", "s", "++"
-			Input.new.multin
-			Sum.new.result
-			$g.reset
-		when "factorial", "f", "!"
-			Input.new.singin
-			Factorial.new.result
-			$g.reset
-		when " ", "exit"
-			print $clear
-			$input = " "
-		end
-	end
-end
-Calculator.new
+g = Calculator.new
+g.explain
+g.run
