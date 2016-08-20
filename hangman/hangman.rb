55d930164984e1cedb7871bb76b959766abd9efb
Tom Lamers <skilletfan59@yahoo.com>
2016-08-20 16:25:21 -0500

    finish hangman game with save and load

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -3,36 +3,148 @@ require_relative "input"
 class Hangman
 
 	include Input
+	attr_accessor :name
 	private
 
-	def initialize
+	def initialize(name)
 		dictionary = File.readlines "words.txt"
 		word_bank = dictionary.select {|word| word.chomp!.length > 4 && word.length < 13}
-		@answer = word_bank.sample
+		@lose = word_bank.sample.downcase
+		@answer = @lose.split.join
 		@display = @answer.gsub(/\w/, "_")
-		@name = get_name
+		@name = name
+		@number_wrong = 0
+		@wrong_guesses = []
+		@gameboard = Drawing.new
+	end
+
+	def won?
+		@lose == @display
 	end
 
 	public
 
-
+	def play
+		while @number_wrong < 6
+			guess = nil
+			print %x{clear}
+			puts @gameboard.draw(@number_wrong)
+			puts "Wrong guesses: " + @wrong_guesses.join(", ") if not @wrong_guesses.empty?
+			puts "\n\n     #{@display}\n\n\n"
+			until guess != nil
+				print "What letter would you like to guess, #{@name}?  "
+				guess = get_guess
+				if guess !=nil && (@display.include?(guess) || @wrong_guesses.include?(guess))
+					guess = nil
+					puts "You already guessed that letter!"
+				end
+			end
+			break if guess == "save"
+			if @answer.include?(guess)
+				until @answer.include?(guess) == false
+					position = @answer.index(guess)
+					@display[position] = guess
+					@answer.sub!(guess, "*")
+				end
+			else
+				@wrong_guesses << guess
+				@number_wrong += 1
+			end
+			break if won?
+		end
+		if guess == "save"
+		elsif won?
+			print %x{clear}
+			puts @gameboard.draw(@number_wrong)
+			puts "Wrong guesses: " + @wrong_guesses.join(", ") if not @wrong_guesses.empty?
+			puts "\n\n     #{@display}\n\n\n"
+			puts "Congratulations #{@name}, You Win!!"
+		else
+			print %x{clear}
+			puts @gameboard.draw(@number_wrong)
+			puts "Wrong guesses: " + @wrong_guesses.join(", ") if not @wrong_guesses.empty?
+			puts "\n\n     #{@display}\n\n\n"
+			puts "Unfortunately you were unable to guess the word and killed the guy..."
+			puts "The correct answer is #{@lose}."
+		end
+		guess
+	end
 end
-Hangman.new
 
-a = "\u2503"
-b = "\u2501"
-c = "\u250F"
-d = "\u2513"
-e = "\u253B"
-g = "\u2502"
-h = "\u2524"
-i = "\u2570"
-j = "\u253C"
-k = "\u256F"
-l = "\u2534"
-m = "\u256D"
-n = "\u256E"
-p = "\u03D9"
-f = "\u2620"
-#puts "  #{p}\n #{m}#{j}#{n}\n #{m}#{l}#{n}"
-puts " #{c}#{b}#{b}#{d}\n #{a}  #{f}\n #{a} #{m}#{j}#{n}\n #{a} #{m}#{l}#{n}\n#{b}#{e}#{b}" + "   #{q}#{r}#{s}"
+class Drawing
+	def initialize
+		@a = "\u2503"
+		@b = "\u2501"
+		@c = "\u250F"
+		@d = "\u2513"
+		@e = "\u253B"
+		@g = "\u2502"
+		@h = "\u2524"
+		@i = "\u2570"
+		@j = "\u253C"
+		@k = "\u256F"
+		@l = "\u2534"
+		@m = "\u256D"
+		@n = "\u256E"
+		@p = "\u03D9"
+		@f = "\u2620"
+	end
+
+	def draw(num)
+		if num == 0
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}\n #{@a}\n #{@a}\n#{@b}#{@e}#{@b}"
+		elsif num == 1
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@p}\n #{@a}\n #{@a}\n#{@b}#{@e}#{@b}"
+		elsif num == 2
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@p}\n #{@a}  #{@g}\n #{@a}\n#{@b}#{@e}#{@b}"
+		elsif num == 3
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@p}\n #{@a} #{@i}#{@h}\n #{@a}\n#{@b}#{@e}#{@b}"
+		elsif num == 4
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@p}\n #{@a} #{@i}#{@j}#{@k}\n #{@a}\n#{@b}#{@e}#{@b}"
+		elsif num == 5
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@p}\n #{@a} #{@i}#{@j}#{@k}\n #{@a} #{@m}#{@l}\n#{@b}#{@e}#{@b}"
+		elsif num == 6
+			drawing = " #{@c}#{@b}#{@b}#{@d}\n #{@a}  #{@f}\n #{@a} #{@m}#{@j}#{@n}\n #{@a} #{@m}#{@l}#{@n}\n#{@b}#{@e}#{@b}"
+		end
+		drawing
+	end
+end
+
+class Run
+
+	include Input
+	include Game_functions
+
+	def initialize
+		print %x{clear}
+		puts "Welcome to Hangman, Created by Tom Lamers!"
+		option = get_answer
+		if option == "new"
+			name = get_name
+			run(name)
+		elsif option == "load"
+			game = load
+			if game == "exit"
+				print %x{clear}
+				name = get_name
+				run(name)
+			else
+				function = game.play
+				if function == "save"
+					save(game)
+					close
+				elsif play_again?
+					name = game.name
+					run(name)
+				else
+					close
+				end
+			end
+		end
+	end
+end
+Run.new
+
+	
+
+
--- hangman/input.rb
+++ hangman/input.rb
@@ -5,4 +5,129 @@ module Input
 		print %x{clear}
 		name
 	end
+
+	def get_guess
+		guess = gets.chomp.downcase
+		if guess == "save"
+		else	
+			if guess.length > 1
+				puts "A valid guess must only be one letter"
+				guess = nil
+			elsif guess.scan(/\d/).empty? == false
+				puts "You are not allowed to guess numbers!"
+				guess = nil
+			end
+		end
+		guess
+	end
+
+	def get_answer
+		entry = nil
+		until entry != nil
+			print "\nWould you like to start a new game (new) or load a saved game (load)? "
+			entry = case gets.chomp.downcase
+				when "new", "n" then "new"
+				when "load", "l" then "load"
+				else nil
+			end
+			puts "That is not a valid option, new or load?" if entry.nil?
+		end
+		entry
+	end
+
+	def play_again?
+		entry = nil
+		until entry != nil
+			print "\nWould you like to play again? "
+			entry = case gets.chomp.downcase
+				when "yes", "y" then true
+				when "no", "n" then false
+				else nil
+			end
+			puts "That is not a valid option, yes or no" if entry.nil?
+		end
+		entry
+	end
+end
+
+module Game_functions
+	require "yaml"
+	def run(name)
+		play = true
+		until not play
+			game = Hangman.new(name)
+			function = game.play
+			if function == "save"
+				save(game)
+				play = false
+			else
+				play = play_again?
+			end
+		end
+		close
+	end
+
+	def save(game)
+		Dir.mkdir("saved_games") unless Dir.exists? "saved_games"
+		look = true
+		while look == true
+			print "\nPlease enter a unique identifier used to load your game again: "
+			id = gets.chomp.downcase
+			until id != "exit"
+				print "\nYou won't be able to load it with exit. Try a different id: "
+				id = gets.chomp.downcase
+			end
+			filename = "saved_games/saved_game_#{id}.txt"
+			if File.file?(filename) == false
+				File.open(filename, 'w') {|f| f.write(YAML.dump(game)) }
+				print "Saving Game."
+				sleep(1)
+				print "."
+				sleep(1)
+				print "."
+				sleep(1)
+				puts "Game has succesfully saved!"
+				puts
+				look = false
+			else
+				puts "There is already a game saved with that id, try a different one!"
+			end
+		end
+	end
+
+	def load
+		Dir.mkdir("saved_games") unless Dir.exists? "saved_games"
+		look = true
+		while look == true
+			print "\nWhat is the unique identifier you saved your game with? "
+			id = gets.chomp.downcase
+			if id == "exit"
+				game = "exit"
+				look = false
+			else
+				filename = "saved_games/saved_game_#{id}.txt"
+				if File.file?(filename)
+					game = YAML.load(File.read(filename))
+					print %x{clear}
+					print "Loading Game."
+					sleep(1)
+					print "."
+					sleep(1)
+					print "."
+					sleep(1)
+					look = false
+					File.delete(filename)
+				else
+					puts "There is no saved game with that id, please try again..."
+				end
+			end
+		end
+		game
+	end
+
+	def close
+		puts "Thanks for playing!"
+		sleep(3)
+		print %x{clear}
+	end
 end
